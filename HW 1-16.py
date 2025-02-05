56e2fa9ab0c10dc703e21a24ecef4f342ff6144d
Viktar <minskmfi@gmail.com>
2025-02-05 22:55:51 +0300

    Update HW 1-16.py
    "Delete HW 1-16.py"

--- HW 1-16.py
+++ HW 1-16.py
@@ -1,9 +1,1 @@
-# Создаем список чисел от 1 до 20
-original_list = list(range(1, 21))
 
-# Фильтруем четные числа
-even_list = [x for x in original_list if x % 2 == 0]
-
-# Выводим результаты
-print("Исходный список:", original_list)
-print("Список четных чисел:", even_list)
